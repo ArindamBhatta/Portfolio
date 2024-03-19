6d8dd4fbacaafa6d8a46d5893cf2e7895a4e1f69
Arindam Bhattacharyya <arindambhattacharyya.ab@gmail.com>
2024-03-19 10:50:19 +0530

    production error

--- src/Components/Github/Github.tsx
+++ src/Components/Github/Github.tsx
@@ -1,4 +1,23 @@
 import "./Github.css"
+interface UserData {
+    avatar_url: string;
+    name: string;
+    bio: string;
+    location: string;
+    company: string;
+    public_repos: number;
+    followers: number;
+    repos_url: string;
+}
+
+interface RepoData {
+    id: number;
+    name: string;
+    language: string;
+    clone_url: string;
+    watchers: number;
+}
+
 import { PushpinOutlined,
         AimOutlined,
         UsergroupAddOutlined,
@@ -11,18 +30,17 @@ import { PushpinOutlined,
 import { useEffect, useState } from 'react';
 
 export default function Github() {
-    const [user, setUser] = useState(null);
-    const [repos, setRepos] = useState([]);
+    const [user, setUser] = useState<UserData | null>(null);
+    const [repos, setRepos] = useState<RepoData[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/users/ArindamBhatta')
         .then(res => res.json())
-        .then(userData => {
-            console.log(userData);
+        .then((userData: UserData) => { // Type assertion here
             setUser(userData);
             fetch(userData.repos_url)
                 .then(res => res.json())
-                .then(repoData => {
+                .then((repoData: RepoData[]) => { // Type assertion here
                     setRepos(repoData);
                 });
         });
@@ -52,10 +70,10 @@ export default function Github() {
                         {repos.map(repo => (
                                 <li key={repo.id} className="all-reposetory">
                                     <div className="position">
-                                        <p> <FolderOpenOutlined /> Repositorie name: - <span> {repo.name} </span> </p>   
-                                        <p>  <CodeOutlined /> Language name: - <span> {repo.language} </span></p> 
-                                        <p> <BranchesOutlined /> Clone Url: - <span> {repo.clone_url}</span> </p>
-                                        <p> <EyeOutlined /> Watchers <span> {repo.watchers} </span> </p>
+                                        <p> <FolderOpenOutlined /> Repositorie name: - <span> {repo?.name} </span> </p>   
+                                        <p>  <CodeOutlined /> Language name: - <span> {repo?.language} </span></p> 
+                                        <p> <BranchesOutlined /> Clone Url: - <span> {repo?.clone_url}</span> </p>
+                                        <p> <EyeOutlined /> Watchers <span> {repo?.watchers} </span> </p>
                                     </div>
                                 </li>
                         ))}
